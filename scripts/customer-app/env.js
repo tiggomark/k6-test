953d86f5e51b92f18d12a415aff2fc99c6cd9bf2
thomazrd <thomaz.damasceno-ext@via.com.br>
2022-07-31 08:45:18 -0300

    Update readme

--- scripts/customer-app/env.js
+++ scripts/customer-app/env.js
@@ -1,6 +1,6 @@
 
 export let env =  {
-    baseUrl: "http://52.87.173.22/:8081/v1",
+    baseUrl: "http://52.87.173.22:8081/v1",
     defaultUsername: "teste@gmail.com",
     defaultEmail: "teste@gmail.com",
     defaultPassword: "default_password"
