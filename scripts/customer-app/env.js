1499cd835ef7d427f6dc67904d55100b4a02861e
thomazrd <thomaz.damasceno-ext@via.com.br>
2022-08-02 08:53:55 -0300

    Update readme

--- scripts/customer-app/env.js
+++ scripts/customer-app/env.js
@@ -1,6 +1,6 @@
 
 export let env =  {
-    baseUrl: "http://52.87.173.22:8081/v1",
+    baseUrl: "http://default-load-balance2-1519609720.us-east-1.elb.amazonaws.com:8081/v1",
     defaultUsername: "teste@gmail.com",
     defaultEmail: "teste@gmail.com",
     defaultPassword: "default_password"
